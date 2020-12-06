044e5c9f52979592b98921414470f3b785b5a6a3
marcelocaldeiracosta <marcelo.andrade_tdsoft@prestador.globo>
2020-12-06 09:18:40 -0300

    criacao da logica de busca das receitas e retorno no formato solicitado

--- src/config/index.js
+++ src/config/index.js
@@ -5,7 +5,8 @@ const config = {
   HOST: process.env.SERVER_HOST || "localhost",
   PORT: process.env.SERVER_PORT != null ? Number(process.env.SERVER_PORT) : 5678,
   API_RECIPE: process.env.API_RECIPE || "http://www.recipepuppy.com/api/",
-  API_GLIPHY: process.env.API_GLIPHY || "http://api.giphy.com/v1/gifs/search"
+  API_GLIPHY: process.env.API_GLIPHY || "https://api.giphy.com/v1/gifs/search",
+  API_KEY_GLIPHY: process.env.API_KEY_GLIPHY || "16YaRZLTubS07GpSMq3QnjHyQ0Qj5EGK"
 };
 
 module.exports = {
--- src/controllers/delivery-much-controller.js
+++ src/controllers/delivery-much-controller.js
@@ -1,5 +1,6 @@
 const { config } = require("../config");
 const axios = require("axios");
+const { json } = require("body-parser");
 let msgError = null;
 
 class DeliveryMuchController {
@@ -7,19 +8,65 @@ class DeliveryMuchController {
   constructor() {
   }
 
+  async getGif(title) {
+    try {
+      const response = await axios.get(config.API_GLIPHY, {
+        params: {
+          api_key: config.API_KEY_GLIPHY,
+          q: title,
+          limit: 1
+        }
+      });
+      return response.data.data.length > 0 ? response.data.data[0].images.original.url : "Gif não encontrado";
+    } catch(error) {
+      throw `Error: ${error}`;
+    }
+  }
+
+  transformToArray(ingredients) {
+    const array = ingredients.split(", ").filter(i => i).sort();
+    // Remove duplicados
+    return [...new Set(array)];
+  }
+
   async getRecipesWs(ingredients) {
-    const uriRecipe = `${config.API_RECIPE}?i=${ingredients}`;
-    const response = await axios.get(uriRecipe);
+    let receitas = [];
+    const response = await axios.get(config.API_RECIPE, {
+      params: {
+        i: ingredients
+      }
+    });
 
     if (response.status !== 200) {
-      msgError = `[WORKER] - Falha ao receber os dados da API de material tradicional: ${response.statusText}`;    
+      msgError = `Erro ao pesquisar receitas: ${response.statusText}`;
+      return { receitas, msgError };
     }
 
-    const receitas = response.data.results != null ? response.data.results : [];
+    const dataReceived =  response.data.results != null ? response.data.results : [];
+
+    // Usado o for por causa do await do getGif
+    for (const r of dataReceived) {
+      const rTitle = r.title.trim();
+      const gif = await this.getGif(rTitle);
+      const ingredients = this.transformToArray(r.ingredients);
+
+      receitas.push({
+        title: rTitle,
+        ingredients,
+        link: r.href,
+        gif
+      });
+    }
+
+    receitas = {
+      keywords: this.transformToArray(ingredients),
+      recipes: receitas
+    };
+
     return { receitas, msgError };
   }
 }
 
 module.exports = {
   deliveryMuchController: new DeliveryMuchController()
-}
+};
